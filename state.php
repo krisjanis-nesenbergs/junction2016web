c8d8317d5960808c18f8addff86bba96c866464a
krisjanis-nesenbergs <krisjanis.nesenbergs@gmail.com>
2016-11-26 17:30:46 +0200

    Sql try 2

--- state.php
+++ state.php
@@ -1,5 +1,26 @@
 <?php
-include 'active.php';
-echo $empl;
+$svalue = $_SERVER["MYSQLCONNSTR_localdb"];
+$servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $svalue);
+$dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $svalue);
+$username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $svalue);
+$password = preg_replace("/^.*Password=(.+?)$/", "\\1", $svalue);
 
+
+file_put_contents('filename.php', 0);
+$conn = new mysqli($servername, $dbname, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT user FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo $row["user"];
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
